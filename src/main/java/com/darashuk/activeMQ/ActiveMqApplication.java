a3a18c3ced325dd97d3a2238987c2bf8f37af37f
Dzmitry_Darashuk <itDarashukd@gmail.com>
2021-06-02 15:19:31 +0300

    work app - ready to .jar\without TOMCAT

--- src/main/java/com/darashuk/activeMQ/ActiveMqApplication.java
+++ src/main/java/com/darashuk/activeMQ/ActiveMqApplication.java
@@ -1,15 +1,15 @@
 package com.darashuk.activeMQ;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
 @EnableJms
 public class ActiveMqApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ActiveMqApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ActiveMqApplication.class).web(WebApplicationType.NONE).run(args);
+    }
 }
